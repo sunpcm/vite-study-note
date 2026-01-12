371c0d8fcbeedff693cc1672bfd0863e437dcbc4
sunpcm <sunpcm@163.com>
2026-01-12 17:00:36 +0800

    feat: integrate Tailwind CSS and update styling in App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './styles.scss'
 
-const root = createRoot(document.getElementById('root') as HTMLBRElement)
+import './index.css'
+
+const root = createRoot(document.getElementById('root') as HTMLDivElement)
 root.render(<App />)
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     // 插件
     plugins: [
         react(), // 提供 Fast Refresh 和 JSX 环境
+        tailwindcss(),
+        // autoprefixer(),
         visualizer({
             open: true,  // 构建完自动打开
             gzipSize: true, // 显示 gzip 大小
@@ -47,16 +50,6 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src')
-        }
-    },
-
-    // css 预处理
-    // 不用配loader，如果想注入全局 scss变量，可以在这里配置
-    css: {
-        preprocessorOptions: {
-            scss: {
-                // additionalData: `@import "@/styles/variables.scss";`
-            }
         }
     },
 
